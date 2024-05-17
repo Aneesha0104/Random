88a47469e558b838ca59aaffb872a30038bf5a1f
Aneesha <97899173+Aneesha0104@users.noreply.github.com>
2024-05-17 16:22:47 +0530

    Add files via upload

--- extraction.py
+++ extraction.py
@@ -1,18 +1,18 @@
 import nltk
-import random
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.classify import NaiveBayesClassifier
-from nltk import ne_chunk, pos_tag
-import re
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import spacy
 
 # Read data from CSV file
 file_path = "Train.csv"
 data = pd.read_csv(file_path, encoding='ISO-8859-1')
 
-# Function to extract features from text
+file_path = "doc1"
+with open(file_path, 'r') as file:
+    test_data = file.read()
 def extract_features(document):
     words = set(word_tokenize(document))
     features = {word: True for word in words}
@@ -20,34 +20,31 @@ def extract_features(document):
 
 labeled_data = [(extract_features(row['text']), row['label']) for _, row in data.iterrows()]
 
+nlp = spacy.load("en_core_web_sm")
+
+# Function to extract countries from text using spacy
 def extract_countries(text):
-    entities = ne_chunk(pos_tag(word_tokenize(text)))
-    countries = []
-    country_pattern = re.compile(r'((?:[A-Z][a-z]+ ?)+)')
-
-
-    for entity in entities:
-        if hasattr(entity, 'label') and entity.label() == 'GPE':
-            name = ' '.join(word for word, _ in entity.leaves())
-            if country_pattern.match(name):
-                countries.append(name)
+    doc = nlp(text)
+    countries = [ent.text for ent in doc.ents if ent.label_ == 'GPE']
     return countries
 
-# Split data into training and testing sets using train_test_split
+def extract_countries(text):
+    countries = []
+    doc = nlp(text)
+    for ent in doc.ents:
+        if ent.label_ == 'GPE':
+            countries.append(ent.text)
+    return countries
+
 train_set, test_set = train_test_split(labeled_data, test_size=0.2, random_state=42)
 
-# Train the Naive Bayes classifier
+# Train
 classifier = NaiveBayesClassifier.train(train_set, estimator=nltk.probability.LaplaceProbDist)
 
-# Test the classifier
+# Test
 print("Accuracy:", nltk.classify.accuracy(classifier, test_set))
 
-# Read test data from file
-file_path = "doc1"
-with open(file_path, 'r') as file:
-    test_data = file.read()
-
-# Tokenize the test data into sentences
+# Tokenize
 sentences = sent_tokenize(test_data)
 
 # Initialize a list to store sentences labeled as exclusion along with the extracted countries
@@ -61,7 +58,7 @@ for sentence in sentences:
         countries = extract_countries(sentence)
         exclusion_with_countries.append((sentence, countries))
 
-# Print sentences labeled as exclusion along with extracted countries
+
 for sentence, countries in exclusion_with_countries:
     print("Sentence:", sentence)
     print("Countries:", countries)
